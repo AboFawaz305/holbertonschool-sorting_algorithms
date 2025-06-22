79d083372e6afe9c347d5ef15bf0c9c9a4555ae1
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-06-22 11:26:23 +0300

    ADDED task 0 solution

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * swap - swap elements i and j
+ * @array: an array
+ * @i: the first element
+ * @j: the second element
+ */
+void swap(int *array, int i, int j)
+{
+	int temp = array[i];
+
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+/**
+ * bubble_sort - sort array using bubble sort
+ * @array: an array of numbers to sort
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	if (array == NULL)
+		return;
+	for (i = size; i >= 1; i--)
+		for (j = 1; j < i; j++)
+			if (array[j - 1] > array[j])
+				swap(array, j - 1, j);
+}
+
