f1655a2677df400bf37fb1b854d45d12aafc6088
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-06-22 11:47:20 +0300

    ADDED print every state of the array while ordering it

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,13 +5,15 @@
  * @array: an array
  * @i: the first element
  * @j: the second element
+ * @size: the size of array
  */
-void swap(int *array, int i, int j)
+void swap(int *array, int i, int j, int size)
 {
 	int temp = array[i];
 
 	array[i] = array[j];
 	array[j] = temp;
+	print_array(array, size);
 }
 
 /**
@@ -28,6 +30,6 @@ void bubble_sort(int *array, size_t size)
 	for (i = size; i >= 1; i--)
 		for (j = 1; j < i; j++)
 			if (array[j - 1] > array[j])
-				swap(array, j - 1, j);
+				swap(array, j - 1, j, size);
 }
 
